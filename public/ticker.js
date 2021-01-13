c408048b455671910d9ecd923c1f06fc5b13fe0e
Colin Mitchell <muffinista@gmail.com>
2021-01-13 16:49:45 -0500

    Cleanup code

--- public/ticker.js
+++ public/ticker.js
@@ -1,31 +1,31 @@
-// random array sort from
-// http://javascript.about.com/library/blsort2.htm
-var randOrd = function() { return(Math.round(Math.random())-0.5); }
-
-var getLocation = function(href) {
-  var l = document.createElement("a");
-  l.href = href;
-  return l.protocol + "//" + l.host;
-};
-
-var shuffleList = function(ul) {
-  for (var i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[Math.random() * i | 0]);
-  }
-};
-
-var setupAnimation = function(el) {
-  el.classList.add("fade-in-and-out");
-  el.addEventListener('animationend', () => {
-    el.classList.remove("fade-in-and-out");
-    setTimeout(function() {
-      el.classList.add("fade-in-and-out");
-      shuffleList(el);
-    }, 100);
-  });
-};
-
 var setupTicker = function() {
+  // random array sort from
+  // http://javascript.about.com/library/blsort2.htm
+  var randOrd = function() { return(Math.round(Math.random())-0.5); }
+  
+  var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l.protocol + "//" + l.host;
+  };
+  
+  var shuffleList = function(ul) {
+    for (var i = ul.children.length; i >= 0; i--) {
+      ul.appendChild(ul.children[Math.random() * i | 0]);
+    }
+  };
+  
+  var setupAnimation = function(el) {
+    el.classList.add("fade-in-and-out");
+    el.addEventListener('animationend', () => {
+      el.classList.remove("fade-in-and-out");
+      setTimeout(function() {
+        el.classList.add("fade-in-and-out");
+        shuffleList(el);
+      }, 100);
+    });
+  };
+  
   var host = getLocation(document.getElementById("ticker").src);
   
   historyData.load({
@@ -38,8 +38,10 @@ var setupTicker = function() {
        
         
       document.querySelector("#today").innerHTML = "Today in History: " + d.date;
-      document.querySelector("#learn-more").innerHTML = d.date + " on Wikipedia";
-      document.querySelector("#learn-more").href = d.url;
+      let lm = document.querySelector("#learn-more");
+      
+      lm.innerHTML = d.date + " on Wikipedia";
+      lm.href = d.url;
         
       let births = document.querySelector("#births");
       let deaths = document.querySelector("#deaths");
