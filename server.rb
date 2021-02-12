aa8aba405b38f5c36fd3d9a62b074b53dcf94556
Colin Mitchell <muffinista@gmail.com>
2021-02-12 14:32:17 -0500

    Restore /date route

--- server.rb
+++ server.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'date'
+
 get('/') { send_file File.expand_path('index.html', settings.public_folder)  }
+
+get('/date') {
+  content_type :json, 'charset' => 'utf-8'
+
+  today = Date.today
+  render_data(today.month, today.day)
+}
+
 get('/date/:month/:day') {
   content_type :json, 'charset' => 'utf-8'
-  filename = "#{'%02d' % params['month'].to_i}-#{'%02d' % params['day'].to_i}.json"
+  render_data(params[:month], params[:day])
+}
+
+
+def render_data(month, day)
+  filename = "#{'%02d' % month.to_i}-#{'%02d' % day.to_i}.json"
   path = File.expand_path(File.join('data', filename), settings.public_folder)
   if params['callback']
     data = File.read(path)
@@ -11,4 +26,4 @@ get('/date/:month/:day') {
   else
     send_file path
   end
-}
+end
